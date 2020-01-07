e775df19d10867b4053b4f85f4fd92caf1f77f09
dcpushparaj <dcpushparaj@gamil.com>
2020-01-07 14:26:14 +0545

    initial init

---
+++ DatingApp.APi/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.APi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.APi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
